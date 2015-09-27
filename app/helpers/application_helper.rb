b25e6746b18724d32d5eabc187da17ab4b078d63
anhnvbk93 <anh.nguyen.it.dut@gmail.com>
2015-09-28 00:30:26 +0700

    [Add] Add favicon and page title on each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
   end
+
+  # returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "My Recipe App - AnhNV";
+    if  page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
